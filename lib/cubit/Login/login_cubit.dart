31edb696b1c646d777d10c1a8705805801988ff0
Salma Ahmed <158223837+Salma-Ahmed3@users.noreply.github.com>
2024-08-28 22:37:52 +0300

    Update login_cubit

--- lib/cubit/Login/login_cubit.dart
+++ lib/cubit/Login/login_cubit.dart
@@ -1,32 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:nowproject/cubit/Login/login_state.dart';
-import 'package:nowproject/services/app_services.dart';
+
+import '../../services/services.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
 
   Future<void> login(String email, String password) async {
     emit(LoginLoading());
-      final body = {
-        'userName': email,
-        'password': password,
-      };
-      final response = await AppService.callService(
-        actionType: ActionType.post,
-        apiName: 'Account/Login',
-        body: body,
-      );
-      
-        if (email == '0588638638' && password == '111111') {
-        emit(LoginSuccess(
-          message: response.toString(),
-        ));
-        }
-        else if(email != body && password !=body){
-        emit(LoginFailuer(
-        message: ' رقم الجوال او كلمة المرور خاطئة'
-        ));
-        
+    final body = {
+      'userName': email,
+      'password': password,
+    };
+    final response = await AppService.callService(
+      actionType: ActionType.post,
+      apiName: 'Account/Login',
+      body: body,
+    );
+
+    if (email == '0588638638' && password == '111111') {
+      emit(LoginSuccess(
+        message: response.toString(),
+      ));
+    } else if (email != body && password != body) {
+      emit(LoginFailuer(message: ' رقم الجوال او كلمة المرور خاطئة'));
     }
   }
 }
